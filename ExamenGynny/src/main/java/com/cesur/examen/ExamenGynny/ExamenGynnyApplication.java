886d471e013435b4dcbb76da6f6ba7b2aa507878
Acner Mendez Feliz <136130876+Acner01@users.noreply.github.com>
2023-11-07 20:55:25 +0100

    Primer Commit

---
+++ ExamenGynny/src/main/java/com/cesur/examen/ExamenGynny/ExamenGynnyApplication.java
@@ -0,0 +1,13 @@
+package com.cesur.examen.ExamenGynny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenGynnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenGynnyApplication.class, args);
+	}
+
+}
---
+++ ExamenGynny/src/test/java/com/cesur/examen/ExamenGynny/ExamenGynnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesur.examen.ExamenGynny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenGynnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
